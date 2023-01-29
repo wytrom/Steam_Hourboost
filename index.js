c396de3c4272f5b2b35112545e1ebce6ab2363a7
wytrom <53312166+wytrom@users.noreply.github.com>
2023-01-28 21:26:15 -0300

    Update 1.0
    Adicionado um contador de tempo online do bot e um log de quanto tempo ficou online quando o bot ficou offline.
    Adicionado a possibilidade de controlar o bot através de comandos de mensagem Steam, como !start e !stop.
    Adicionado a exibição da data e hora de Brasília quando o bot informa o horário de início e fim no console.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js')
+const moment = require('moment-timezone');
+const keep_alive = require('./keep_alive.js');
 
 var accounts = [
     {username: 'Seu login', password: 'Sua senha', shared_secret: 'Seu Secret key'},
@@ -10,22 +11,39 @@ var accounts = [
 
 var games = [730, 440, 570];  // Enter here AppIDs of the needed games
 var status = 1;  // 0 - Offline, 1 - Online, 7 - Invisible
+var running = true;
 
 for (let account of accounts) {
     let user = new steamUser();
+    let startTime = moment().tz("America/Sao_Paulo").format(); 
     user.logOn({
         "accountName": account.username, 
         "password": account.password, 
         "twoFactorCode": steamTotp.generateAuthCode(account.shared_secret)
     });
     user.on('loggedOn', () => {
+        console.log(`Iniciado em: ${startTime}`);
         if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
         user.setPersona(status);               
         user.gamesPlayed(games);
-    });
-
-    user.on('friendMessage', (steamID, message) => {
-        // respond with a funny message
-        user.chatMessage(steamID, "Não estou Online! Apenas usando Akuma Boost, Para Aumentar as horas do perfil");
+        user.on('friendMessage', (steamID, message) => {
+            if(message === '!start' && running === false) {
+                running = true;
+                startTime = moment().tz("America/Sao_Paulo").format(); 
+                console.log(`Iniciado em: ${startTime}`);
+                user.setPersona(status);               
+                user.gamesPlayed(games);
+                user.chatMessage(steamID, "Bot iniciado com sucesso!");
+            } else if(message === '!stop' && running === true) {
+                running = false;
+                let endTime = moment().tz("America/Sao_Paulo").format();
+                console.log(`Finalizado em: ${endTime}`);
+                user.setPersona(0);
+                user.gamesPlayed([]);
+                user.chatMessage(steamID, "Bot parado com sucesso!");
+            } else {
+                user.chatMessage(steamID, "Não estou Online! Apenas usando Akuma Boost, Para Aumentar as horas do perfil");
+            }
+        });
     });
 }
